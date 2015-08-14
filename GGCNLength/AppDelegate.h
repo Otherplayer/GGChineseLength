301fb5aa2d30a59e228dfcb9bf8adc1e71c29434
__fqah_ <Otherplayer@GuoledeMBP.VAP7300>
2015-08-14 16:49:10 +0800

    Initial Commit

---
+++ GGCNLength/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GGCNLength
+//
+//  Created by __fqah_ on 15/8/14.
+//  Copyright (c) 2015年 __fqah_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GGCNLength/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GGCNLength
+//
+//  Created by __fqah_ on 15/8/14.
+//  Copyright (c) 2015年 __fqah_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
