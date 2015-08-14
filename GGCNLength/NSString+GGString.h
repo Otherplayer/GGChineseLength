0ef71f3329ab49f9c2ed9cff25ff755d09a2d49b
__fqah_ <Otherplayer@GuoledeMBP.VAP7300>
2015-08-14 16:59:12 +0800

    first commit

---
+++ GGCNLength/NSString+GGString.h
@@ -0,0 +1,13 @@
+//
+//  NSString+GGString.h
+//  GGCNLength
+//
+//  Created by __fqah_ on 15/8/14.
+//  Copyright (c) 2015年 __fqah_. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (GGString)
+
+@end
---
+++ GGCNLength/NSString+GGString.m
@@ -0,0 +1,13 @@
+//
+//  NSString+GGString.m
+//  GGCNLength
+//
+//  Created by __fqah_ on 15/8/14.
+//  Copyright (c) 2015年 __fqah_. All rights reserved.
+//
+
+#import "NSString+GGString.h"
+
+@implementation NSString (GGString)
+
+@end
